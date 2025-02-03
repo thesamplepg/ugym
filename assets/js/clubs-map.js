714c9953e7dd96afefa21d4dfb01e559cd331ca1
thesamplepg <thesamplepg@gmail.com>
2025-02-03 19:52:27 +0600

    ready

--- assets/js/clubs-map.js
+++ assets/js/clubs-map.js
@@ -1,9 +1,58 @@
 jQuery(document).ready(function ($) {
-    const map = new mapgl.Map('clubs-map', {
-        center: [55.31878, 25.23584],
-        zoom: 13,
-        key: '04274815-4a04-4be9-bbf3-19ba54e7aabc',
-    });
+    const clubsMap = () => {
+        try {
+            const map = new mapgl.Map('clubs-map', {
+                center: [74.603679, 42.871954],
+                zoom: 12,
+                key: '04274815-4a04-4be9-bbf3-19ba54e7aabc',
+            });
+
+            const locations = $('.clubs-map_locations li')
+                .toArray()
+                .map((location) =>
+                    $(location)
+                        .attr('data-club-location')
+                        .split(',')
+                        .map((coord) => parseFloat(coord)),
+                );
+
+            locations.forEach((location) => {
+                const marker = new mapgl.Marker(map, {
+                    coordinates: location.reverse(),
+                });
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const clubsSingleMap = () => {
+        try {
+            const clubsSingleElement = $('#clubs-single-map');
+
+            if (!clubsSingleElement.length) return;
+
+            const clubsSingleLocation = clubsSingleElement
+                .attr('data-map-location')
+                .split(',')
+                .map((cord) => Number(cord));
+
+            const map = new mapgl.Map('clubs-single-map', {
+                center: clubsSingleLocation.reverse(),
+                zoom: 12,
+                key: '04274815-4a04-4be9-bbf3-19ba54e7aabc',
+            });
+
+            const marker = new mapgl.Marker(map, {
+                coordinates: clubsSingleLocation,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    clubsMap();
+    clubsSingleMap();
 });
 
 // jQuery(document).ready(function ($) {
